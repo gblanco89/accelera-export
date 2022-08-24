aec846f6bcb8b5ec2e93366caee622e2d0045ce3
gblanco89 <gbc809@gmail.com>
2022-08-24 11:47:43 +0300

    Hide more annoying notices on screens

--- includes/end.php
+++ includes/end.php
@@ -58,7 +58,7 @@ $url = $upload_dir['baseurl'] . '/' . $filename;
 $message = "Site info exported successfully. Click <a href='" . $url . "' target='_blank'>here</a> to download your CSV. Or click <a href='tools.php?page=accelera-export'>here</a> to go back.";
 
 // Removing other annoying notices
-echo '<style>.update-nag, .notice-info:not(.accelera-notice), .notice-warning:not(.accelera-notice), .updated, .error, .is-dismissible, .ngg_admin_notice { display: none !important; }</style>';
+echo '<style>.update-nag, .notice-info:not(.accelera-notice), .notice-warning:not(.accelera-notice), .updated, .error, .is-dismissible, .ngg_admin_notice, .sbi_notice, .notice:not(.accelera-notice) { display: none !important; }</style>';
 
 echo '<div class="wrap"><h1>Accelera Export</h1><div class="notice notice-success"><p>' . $message . '</p></div></div>';
 

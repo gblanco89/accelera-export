48c0d26f189612d544bdbc33cc98bb3f0ac38a31
gblanco89 <gbc809@gmail.com>
2022-09-11 12:42:21 +0300

    Detect SG as cache managing

--- includes/modules/file-cache.php
+++ includes/modules/file-cache.php
@@ -19,12 +19,24 @@ if ( $wpoptimize && $accelera_wpoptimizeoptions['enable_page_caching'] > 0 ) {
     $bad_cache_plugins['wp_optimize_cache'] = true;
 }
 
+// Check if we are on SiteGround server (taken from SG Optimizer)
+function is_siteground() {
+    if ( ! empty( ini_get( 'open_basedir' ) ) ) {
+        return 0;
+    }
+    return (int) ( @file_exists( '/etc/yum.repos.d/baseos.repo' ) && @file_exists( '/Z' ) );
+}
+
+
 if ( defined('WPE_CACHE_PLUGIN_BASE') ) { // WP Engine?
     $results_tasks[] = 'C';
     $temp_results_tasks_auxiliar = 'WP Engine';
-} elseif ( array_key_exists( 'x-kinsta-cache', $home_url_headers ) ) { // Kinsta?
+} elseif ( array_key_exists('x-kinsta-cache',$home_url_headers) ) { // Kinsta?
     $results_tasks[] = 'C';
     $temp_results_tasks_auxiliar = 'Kinsta';
+} elseif ( is_siteground() ) { // SiteGround?
+    $results_tasks[] = 'C';
+    $temp_results_tasks_auxiliar = 'SiteGround';
 } elseif ( in_array( true, $bad_cache_plugins ) ) { // Bad cache plugin?
     $results_tasks[] = 'D';
     foreach ( $bad_cache_plugins as $key => $value ) {

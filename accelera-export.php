ae36b84b2e8e76179c414d84bb439df9e2de697a
gblanco89 <gbc809@gmail.com>
2023-01-17 16:08:15 +0200

    Bump up version

--- accelera-export.php
+++ accelera-export.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Accelera Export
 description: Companion app for Accelera Assessment service
-Version: 0.32
+Version: 0.33
 Author: Accelera
 Author URI: https://accelera.autoptimize.com
 License: GPLv2 or later
